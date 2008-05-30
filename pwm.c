a6100895668a95c248ce782d1cdb648e6373f44d
Benjamin Kramer <benny.kra@gmail.com>
2008-05-30 14:21:10 +0200

    add 3 channel pwm

--- pwm.c
+++ pwm.c
@@ -2,24 +2,35 @@
 #include <util/delay.h>
 #include "gammatable.h"
 
-#define PWMFREQ 40		// 1s/100Hz/255 states == 40 us
+#define PWMFREQ 39		// 1s/100Hz/255 states == 39 us
 
 int main(void)
 {
-	uint8_t pulspause = 128;
-	DDRD = 0b11111100;
+	uint8_t pulspause1 = 128, pulspause2 = 0, pulspause3 = 255;
+	DDRD = 0b11110000;
 	PORTD = 0xFF;
 
 	while (1) {
 		PORTD = 0b00000011;
-		for (uint8_t i = 0; i < gammatable[pulspause]; i++)
+		for (uint8_t i = 0; i < 0xFF; i++) {
+			if (i > gammatable[pulspause1])
+				PORTD |= 0b10000000;
+			if (i > gammatable[pulspause2])
+				PORTD |= 0b01000000;
+			if (i > gammatable[pulspause3])
+				PORTD |= 0b00100000;
 			_delay_us(PWMFREQ);
-		PORTD = 0xFF;
-		for (uint8_t i = 0; i < gammatable[255 - pulspause]; i++)
-			_delay_us(PWMFREQ);
-		if ((PIND & 0b00000001) == 0)	// PIND 0 low
-			pulspause++;	// will be optimized to a shift
-		else if ((PIND & 0b00000010) == 0)	// PIND 1 low
-			pulspause--;
+		}
+		if ((PIND & 0b00000001) == 0) { // PIND 0 low
+			pulspause1--;
+		} else if ((PIND & 0b00000010) == 0) {	// PIND 1 low
+			pulspause1++;
+		} else if ((PIND & 0b00000100) == 0) { // PIND 3 low
+			pulspause2--;
+			pulspause3++;
+		} else if ((PIND & 0b00001000) == 0) { // PIND 4 low
+			pulspause3--;
+			pulspause2++;
+		}
 	}
 }
